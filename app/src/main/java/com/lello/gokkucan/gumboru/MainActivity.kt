d0fcf76c18ccc3fb8881635e56d0cbe8442aba5c
goksucan <30368027+goksucan@users.noreply.github.com>
2017-11-10 02:34:19 +0300

    Initial commit

---
+++ app/src/main/java/com/lello/gokkucan/gumboru/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.lello.gokkucan.gumboru
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        loadWebpage()
+    }
+
+    fun loadWebpage() {
+
+        // Enable javascript
+        webview.settings.javaScriptEnabled = true
+
+        webview.loadUrl("http://yunus.hacettepe.edu.tr/~goksucanerkoc/gumball-tic-tac-toe/")
+
+    }
+}
